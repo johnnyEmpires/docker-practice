2e2cb57d23489089c77e58b0ae5f59ada744ed0a
john <imperialjohncarlo@gmail.com>
2021-03-27 19:14:59 +0900

    update sample code

--- sample.py
+++ sample.py
@@ -1,1 +1,12 @@
-print("App running within docker")
+import time
+from basiclog import module_logger
+
+log = module_logger(__name__)
+
+log.info("App running within docker")
+
+for i in reversed(range(1, 6)):
+    log.info(f'end of script in...{i}')
+    time.sleep(1)
+
+log.info('sample app in docker terminated.')
